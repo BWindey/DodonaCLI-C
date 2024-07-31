a7749e7e6686cbf72a03ba9141216576a3bc744b
Bram Windey <windey.bram@gmail.com>
2024-07-31 20:36:01 +0200

    Create isBlank-function and move global root-variable to scoped variable

--- libraries/WiJSON/wiJSON.c
+++ libraries/WiJSON/wiJSON.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
 #include "wiJSON.h"
 
-wiValue* root;
+bool isBlank(const char c) {
+	return c == ' ' || c == '\t' || c == '\n' || c == '\r';
+}
 
 wiValue* parseJSON(char *jsonString) {
-	root = (wiValue*)malloc(sizeof(wiValue));
-	root->_type = WIPAIR;
+	wiValue* root = (wiValue*)malloc(sizeof(wiValue));
 
-	wiPair* pair = (wiPair*)malloc(sizeof(wiPair));
-	pair->key = "Hello, World!\n";
+	unsigned int index = 0;
 
-	root->contents.pairVal = pair;
+	// Remove whitespace
+	while (isBlank(jsonString[index]) && jsonString[index] != '\0') {
+		index++;
+	}
+
 
 	return root;
 }
 
 
-void freeEverything() {
+void freeEverything(wiValue* root) {
 	free(root->contents.pairVal);
 	free(root);
 }
 
 
 int main() {
-	parseJSON("{\"testKey\": \"testVal\"}");
+	wiValue* root = parseJSON("{\"testKey\": \"testVal\"}");
 
 	printf("%s", root->contents.pairVal->key);
-	freeEverything();
+	freeEverything(root);
 }
