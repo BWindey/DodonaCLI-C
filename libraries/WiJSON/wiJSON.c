0d0f53bba787dedf389af4e81e60e17f2e4e4446
Bram Windey <windey.bram@gmail.com>
2024-08-07 08:23:28 +0200

    Parse booleans, and created a utility-function to get the index of the next non-blank char.

--- libraries/WiJSON/wiJSON.c
+++ libraries/WiJSON/wiJSON.c
@@ -7,7 +7,8 @@
 #include "wiJSON.h"
 
 // Forward declarations
-bool isBlank(const char c);
+bool isBlank(const char);
+int jumpBlankChars(const char*, unsigned int);
 
 int parseJSONValue	(const char*, unsigned int, wiValue*);
 int parseJSONInt	(const char*, unsigned int, wiValue*);
@@ -23,10 +24,11 @@ int parseJSONNull	(const char*, unsigned int, wiValue*);
 // For testing purposes, a main function.
 // TODO: Remove this when done testing
 int main() {
-	wiValue* root = parseJSON("null");
+	wiValue* root = parseJSON("false");
 
 	printf("RootValue type: %d\n", root->_type);
-	printf("WINULL: %d\n", WINULL);
+	printf("WINULL: %d\n", WIBOOL);
+	printf("Value: %d\n", root->contents.boolVal);
 	freeEverything(root);
 }
 
@@ -38,12 +40,8 @@ int main() {
 wiValue* parseJSON(const char *jsonString) {
 	wiValue* root = (wiValue*)malloc(sizeof(wiValue));
 
-	unsigned int index = 0;
-
-	// Remove whitespace
-	while (isBlank(jsonString[index]) && jsonString[index] != '\0') {
-		index++;
-	}
+	// Start index at first non-blank char
+	unsigned int index = jumpBlankChars(jsonString, 0);
 
 	parseJSONValue(jsonString, index, root);
 
@@ -101,7 +99,28 @@ int parseJSONInt(const char* jsonString, unsigned int index, wiValue* parent) {
 }
 
 int parseJSONBool(const char* jsonString, unsigned int index, wiValue* parent) {
-	return index;
+	if (strncmp(jsonString + index, "true", 4) == 0) {
+		parent->contents.boolVal = true;
+		index += 4;		// Length of "true"
+
+	} else {
+		assert(strncmp(jsonString + index, "false", 4) == 0);
+		parent->contents.boolVal = false;
+		index += 5;		// Length of "false"
+
+	}
+	parent->_type = WIBOOL;
+
+	char nextCharAfterBool = jsonString[index];
+	assert(
+			isBlank(nextCharAfterBool) 
+			|| nextCharAfterBool == ',' 
+			|| nextCharAfterBool == '}' 
+			|| nextCharAfterBool == ']'
+			|| nextCharAfterBool == '\0'
+	);
+
+	return jumpBlankChars(jsonString, index);
 }
 
 int parseJSONString(const char* jsonString, unsigned int index, wiValue* parent) {
@@ -124,12 +143,29 @@ int parseJSONArray(const char* jsonString, unsigned int index, wiValue* parent) 
 	return index;
 }
 
+/*
+ * Tries to parse 'null' (NULL) and asserts if it isn't the case.
+ * This can only assert on a non-valid json-string
+ *
+ * It sets the value and type of the wiValue* parent, 
+ * and returns the index of the next non-blank character after "null"
+ */
 int parseJSONNull(const char* jsonString, unsigned int index, wiValue* parent) {
 	assert(strncmp(jsonString + index, "null", 4) == 0);
 
 	parent->_type = WINULL;
 	index += 4;
-	return index;
+
+	char nextCharAfterNULL = jsonString[index];
+	assert(
+			isBlank(nextCharAfterNULL) 
+			|| nextCharAfterNULL == ',' 
+			|| nextCharAfterNULL == '}' 
+			|| nextCharAfterNULL == ']'
+			|| nextCharAfterNULL == '\0'
+	);
+
+	return jumpBlankChars(jsonString, index);
 }
 
 void freeEverything(wiValue* root) {
@@ -137,6 +173,18 @@ void freeEverything(wiValue* root) {
 	free(root);
 }
 
+
+/*
+ * Returns the index of the first non-blank character
+ * starting from 'index'
+ */
+int jumpBlankChars(const char* string, unsigned int index) {
+	while (isBlank(string[index]) && string[index] != '\0') {
+		index++;
+	}
+
+	return index;
+}
 
 bool isBlank(const char c) {
 	return c == ' ' || c == '\t' || c == '\n' || c == '\r';
