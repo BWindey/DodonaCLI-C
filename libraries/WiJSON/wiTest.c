6eadb701a3f743fb4816b999b9cff03b7a4a1841
Bram Windey <windey.bram@gmail.com>
2024-08-11 21:39:08 +0200

    Add large test from Dodona

--- libraries/WiJSON/wiTest.c
+++ libraries/WiJSON/wiTest.c
@@ -4,13 +4,15 @@
 #include <string.h>
 
 #include "wiJSON.h"
+#include "../Enrich/enrich.h"
+
 
 void testSimpleValues();
 void testSimpleArray();
 void testSimpleObject();
 
-/* void testObject1();
-void testObject2();
+void testObject1();
+/* void testObject2();
 void testObject3();
 void testObject4(); */
 
@@ -19,12 +21,14 @@ int main() {
 	testSimpleArray();
 	testSimpleObject();
 	
-	/* testObject1();
-	testObject2();
+	testObject1();
+	/* testObject2();
 	testObject3();
 	testObject4(); */
 
-	printf("All tests passed.\n");
+	char message[] = "[BLACK][ON BRIGHT-GREEN]All tests passed.[/]\n";
+	enrich(message);
+	printf("%s", message);
 
 	return 0;
 }
@@ -47,7 +51,10 @@ void testSimpleValues() {
 
 	assert(testFloat->_type == WIFLOAT);
 	assert(testFloat->contents.floatVal == -3.21e5);
-	printf("Simple tests succeeded.\n");
+
+	char message[] = "Simple tests [BRIGHT-GREEN]succeeded[/].\n\n";
+	enrich(message);
+	printf("%s", message);
 }
 
 void testSimpleArray() {
@@ -100,7 +107,10 @@ void testSimpleArray() {
 
 	assert(currentElement != NULL);
 	assert(currentElement->elementVal->_type == WINULL);
-	printf("Simple array test succeeded.\n");
+
+	char message[] = "Simple array tests [BRIGHT-GREEN]succeeded[/].\n\n";
+	enrich(message);
+	printf("%s", message);
 }
 
 void testSimpleObject() {
@@ -192,9 +202,219 @@ void testSimpleObject() {
 	assert(pair4_kv3->value->_type == WISTRING);
 	assert(strcmp(pair4_kv3->value->contents.stringVal, "value4") == 0);
 
-	printf("Simple object tests succeeded.\n");
+	char message[] = "Simple object tests [BRIGHT-GREEN]succeeded[/].\n\n";
+	enrich(message);
+	printf("%s", message);
 }
 
+void testObject1() {
+	// Actual Dodona output
+	printf("Testing Dodona courses...\n");
+	wiValue* testObjectCourses = parseJSON(
+		"["
+		"	{"
+  		"	  \"id\": 3230,"
+  		"	  \"name\": \"Algoritmen en Datastructuren 1\","
+  		"	  \"teacher\": \"Veerle Fack\","
+  		"	  \"color\": \"brown\","
+  		"	  \"year\": \"2023-2024\","
+  		"	  \"visibility\": \"visible_for_institution\","
+  		"	  \"registration\": \"open_for_institution\","
+  		"	  \"created_at\": \"2024-01-10T09:50:53.000+01:00\","
+  		"	  \"updated_at\": \"2024-02-09T11:08:07.000+01:00\","
+  		"	  \"url\": \"https://dodona.be/nl/courses/3230.json\","
+  		"	  \"series\": \"https://dodona.be/nl/courses/3230/series.json\""
+  		"	},"
+  		"	{"
+  		"	  \"id\": 2876,"
+  		"	  \"name\": \"Computergebruik\","
+  		"	  \"teacher\": \"Peter Dawyndt\","
+  		"	  \"color\": \"brown\","
+  		"	  \"year\": \"2023-2024\","
+  		"	  \"visibility\": \"visible_for_institution\","
+  		"	  \"registration\": \"open_for_institution\","
+  		"	  \"created_at\": \"2023-09-16T15:12:14.000+02:00\","
+  		"	  \"updated_at\": \"2023-11-30T15:20:31.000+01:00\","
+  		"	  \"url\": \"https://dodona.be/nl/courses/2876.json\","
+  		"	  \"series\": \"https://dodona.be/nl/courses/2876/series.json\""
+  		"	},"
+  		"	{"
+  		"	  \"id\": 2955,"
+  		"	  \"name\": \"Programmeren\","
+  		"	  \"teacher\": \"Kris Coolsaet\","
+  		"	  \"color\": \"brown\","
+  		"	  \"year\": \"2023-2024\","
+  		"	  \"visibility\": \"visible_for_institution\","
+  		"	  \"registration\": \"open_for_institution\","
+  		"	  \"created_at\": \"2023-09-27T10:04:55.000+02:00\","
+  		"	  \"updated_at\": \"2023-09-27T10:04:55.000+02:00\","
+  		"	  \"url\": \"https://dodona.be/nl/courses/2955.json\","
+  		"	  \"series\": \"https://dodona.be/nl/courses/2955/series.json\""
+  		"	},"
+  		"	{"
+  		"	  \"id\": 3355,"
+  		"	  \"name\": \"Scriptingtalen\","
+  		"	  \"teacher\": \"Peter Dawyndt\","
+  		"	  \"color\": \"brown\","
+  		"	  \"year\": \"2023-2024\","
+  		"	  \"visibility\": \"visible_for_institution\","
+  		"	  \"registration\": \"open_for_institution\","
+  		"	  \"created_at\": \"2024-02-08T14:51:07.000+01:00\","
+  		"	  \"updated_at\": \"2024-02-10T13:43:05.000+01:00\","
+  		"	  \"url\": \"https://dodona.be/nl/courses/3355.json\","
+  		"	  \"series\": \"https://dodona.be/nl/courses/3355/series.json\""
+  		"	},"
+  		"	{"
+  		"	  \"id\": 1662,"
+  		"	  \"name\": \"Computergebruik\","
+  		"	  \"teacher\": \"Peter Dawyndt\","
+  		"	  \"color\": \"indigo\","
+  		"	  \"year\": \"2022-2023\","
+  		"	  \"visibility\": \"visible_for_all\","
+  		"	  \"registration\": \"open_for_all\","
+  		"	  \"created_at\": \"2022-09-12T12:30:37.000+02:00\","
+  		"	  \"updated_at\": \"2023-11-30T15:21:34.000+01:00\","
+  		"	  \"url\": \"https://dodona.be/nl/courses/1662.json\","
+  		"	  \"series\": \"https://dodona.be/nl/courses/1662/series.json\""
+  		"	}"
+		"]"
+	);
+
+	assert(testObjectCourses != NULL);
+	assert(testObjectCourses->_type == WIARRAY);
+
+	wiArrayEl* currentElement = testObjectCourses->contents.arrayVal;
+	assert(currentElement != NULL);
+
+	assert(currentElement->elementVal->_type == WIPAIR);
+	wiPair* pair = currentElement->elementVal->contents.pairVal;
+
+	assert(pair != NULL);
+	assert(strcmp(pair->key, "id") == 0);
+	assert(pair->value->_type == WIINT);
+	assert(pair->value->contents.intVal == 3230);
+	
+	pair = pair->nextPair;
+	assert(pair != NULL);
+	assert(strcmp(pair->key, "name") == 0);
+	assert(pair->value->_type == WISTRING);
+	assert(strcmp(pair->value->contents.stringVal, "Algoritmen en Datastructuren 1") == 0);
+	
+	pair = pair->nextPair;
+	assert(pair != NULL);
+	assert(strcmp(pair->key, "teacher") == 0);
+	assert(pair->value->_type == WISTRING);
+	assert(strcmp(pair->value->contents.stringVal, "Veerle Fack") == 0);
+	
+	pair = pair->nextPair;
+	assert(pair != NULL);
+	assert(strcmp(pair->key, "color") == 0);
+	assert(pair->value->_type == WISTRING);
+	assert(strcmp(pair->value->contents.stringVal, "brown") == 0);
+	
+	pair = pair->nextPair;
+	assert(pair != NULL);
+	assert(strcmp(pair->key, "year") == 0);
+	assert(pair->value->_type == WISTRING);
+	assert(strcmp(pair->value->contents.stringVal, "2023-2024") == 0);
+	
+	pair = pair->nextPair; 	// Visibility
+	assert(pair != NULL);
+	pair = pair->nextPair; 	// Registration
+	assert(pair != NULL);
+	pair = pair->nextPair; 	// Created_at
+	assert(pair != NULL);
+	pair = pair->nextPair; 	// Updated_at
+	assert(pair != NULL);
+
+	pair = pair->nextPair;
+	assert(pair != NULL);
+	assert(strcmp(pair->key, "url") == 0);
+	assert(pair->value->_type == WISTRING);
+	assert(strcmp(pair->value->contents.stringVal, "https://dodona.be/nl/courses/3230.json") == 0);
+	
+	pair = pair->nextPair;
+	assert(pair != NULL);
+	assert(strcmp(pair->key, "series") == 0);
+	assert(pair->value->_type == WISTRING);
+	assert(strcmp(pair->value->contents.stringVal, "https://dodona.be/nl/courses/3230/series.json") == 0);
+	
+	assert(pair->nextPair == NULL);
+
+	currentElement = currentElement->nextElement; 	// Computergebruik
+	assert(currentElement != NULL);
+	assert(currentElement->elementVal->_type == WIPAIR);
+	currentElement = currentElement->nextElement; 	// Programmeren
+	assert(currentElement != NULL);
+	assert(currentElement->elementVal->_type == WIPAIR);
+	currentElement = currentElement->nextElement; 	// Scriptingtalen
+	assert(currentElement != NULL);
+	assert(currentElement->elementVal->_type == WIPAIR);
+	currentElement = currentElement->nextElement; 	// Computergebruik
+	assert(currentElement != NULL);
+	assert(currentElement->elementVal->_type == WIPAIR);
+
+	pair = currentElement->elementVal->contents.pairVal;
+
+	assert(pair != NULL);
+	assert(strcmp(pair->key, "id") == 0);
+	assert(pair->value->_type == WIINT);
+	assert(pair->value->contents.intVal == 1662);
+	
+	pair = pair->nextPair;
+	assert(pair != NULL);
+	assert(strcmp(pair->key, "name") == 0);
+	assert(pair->value->_type == WISTRING);
+	assert(strcmp(pair->value->contents.stringVal, "Computergebruik") == 0);
+	
+	pair = pair->nextPair;
+	assert(pair != NULL);
+	assert(strcmp(pair->key, "teacher") == 0);
+	assert(pair->value->_type == WISTRING);
+	assert(strcmp(pair->value->contents.stringVal, "Peter Dawyndt") == 0);
+	
+	pair = pair->nextPair;
+	assert(pair != NULL);
+	assert(strcmp(pair->key, "color") == 0);
+	assert(pair->value->_type == WISTRING);
+	assert(strcmp(pair->value->contents.stringVal, "indigo") == 0);
+	
+	pair = pair->nextPair;
+	assert(pair != NULL);
+	assert(strcmp(pair->key, "year") == 0);
+	assert(pair->value->_type == WISTRING);
+	assert(strcmp(pair->value->contents.stringVal, "2022-2023") == 0);
+	
+	pair = pair->nextPair; 	// Visibility
+	assert(pair != NULL);
+	pair = pair->nextPair; 	// Registration
+	assert(pair != NULL);
+	pair = pair->nextPair; 	// Created_at
+	assert(pair != NULL);
+	pair = pair->nextPair; 	// Updated_at
+	assert(pair != NULL);
+
+	pair = pair->nextPair;
+	assert(pair != NULL);
+	assert(strcmp(pair->key, "url") == 0);
+	assert(pair->value->_type == WISTRING);
+	assert(strcmp(pair->value->contents.stringVal, "https://dodona.be/nl/courses/1662.json") == 0);
+	
+	pair = pair->nextPair;
+	assert(pair != NULL);
+	assert(strcmp(pair->key, "series") == 0);
+	assert(pair->value->_type == WISTRING);
+	assert(strcmp(pair->value->contents.stringVal, "https://dodona.be/nl/courses/1662/series.json") == 0);
+	
+	assert(pair->nextPair == NULL);
+
+	assert(currentElement->nextElement == NULL);
+
+	freeEverything(testObjectCourses);
+	char message[] = "Dodona courses test [BRIGHT-GREEN]passed[/].\n";
+	enrich(message);
+	printf("%s", message);
+}
 
 
 
