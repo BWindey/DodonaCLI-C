11d93da1c2127fa226116beacda95df67d4f91a4
Bram Windey <windey.bram@gmail.com>
2024-10-13 10:39:14 +0200

    WiTUI: extracted top- and bottomborder-rendering to its own function, and created small time_it-macro (currently rendering 1 frame at 0.8ms)

--- libraries/WiTUI/src/wiRender.c
+++ libraries/WiTUI/src/wiRender.c
@@ -5,6 +5,14 @@
 #include <stdlib.h>
 #include "../include/wiTUI.h"
 
+#include <time.h>
+#define time_it(function_call) { \
+	clock_t begin = clock(); \
+	function_call; \
+	clock_t end = clock(); \
+	printf("Time spent calling function " #function_call ": %fms\n", (double)(1000 * (end - begin)) / CLOCKS_PER_SEC); \
+}
+
 typedef struct terminal_size {
 	int rows;
 	int cols;
@@ -42,55 +50,78 @@ terminal_size get_terminal_size() {
 	return (terminal_size) { max.ws_row, max.ws_col };
 }
 
-void render_window(const wi_window* window, int horizontal_offset) {
-	wi_border border = window->border;
+void calculate_window_widths(const wi_session* session) {
 
-	/* Top border */
+}
+
+void render_window_top_border(const wi_border border, char* title, int width, int horizontal_offset) {
 	cursor_move_horizontal(horizontal_offset);
 	printf("%s", border.focussed_colour);
 	printf("%s", border.corner_top_left);
 
 	/* Print title */
 	int current_width = 0;
-	while (current_width < window->width && window->title[current_width]) {
-		printf("%c", window->title[current_width]);
+	while (current_width < width && title[current_width]) {
+		printf("%c", title[current_width]);
 		current_width++;
 	}
 	/* Fill rest of top-border */
-	while (current_width < window->width) {
+	while (current_width < width) {
 		printf("%s", border.side_top);
 		current_width++;
 	}
 	printf("%s\n", border.corner_top_right);
 
+}
+
+void render_window_bottom_border(const wi_border border, char* footer, int width, int horizontal_offset) {
+	/* Calculate some numbers to be able to right-align the footer */
+	int footer_length = strlen(footer);
+	int footer_padding_length = width - footer_length;
+
+	if (footer_padding_length < 0) {
+		footer_padding_length = 0;
+		footer_length = width;
+	}
+	cursor_move_horizontal(horizontal_offset);
+
+	/* No need to apply colour effect, because that happens at ever line-end
+	 * of the window's content */
+	printf("%s", border.corner_bottom_left);
+	for (int _ = 0; _ < footer_padding_length; _++) {
+		printf("%s", border.side_bottom);
+	}
+
+	/* Restring footer-length if necessary (can't be longer then window-width) */
+	printf("%.*s", footer_length, footer);
+
+	printf("%s\033[0m\n", border.corner_bottom_right);
+}
+
+void render_window(const wi_window* window, int horizontal_offset) {
+	wi_border border = window->border;
+
+	render_window_top_border(
+		border, window->title,
+		window->_internal_rendered_width, horizontal_offset
+	);
+
 	/* Contents (empty for now) */
 	for (int i = 0; i < window->height; i++) {
 	cursor_move_horizontal(horizontal_offset);
 		printf("%s", border.side_left);
-		for (int j = 0; j < window->width; j++) {
-			printf("\033[0m%c%s", 'c', border.focussed_colour); 		/* This will need to print content */
+		for (int j = 0; j < window->_internal_rendered_width; j++) {
+			/* This will need to print content,
+			 * also applies colour to "closing" border */
+			printf("\033[0m%c%s", 'c', border.focussed_colour);
 		}
 		printf("%s\n", border.side_right);
 	}
 
-	/* Print footer, some calculations because it is right-aligned */
-	current_width = 0;
-	int footer_length = strlen(window->footer);
-	int footer_padding_length = window->width - footer_length;
-
-	if (footer_padding_length < 0) {
-		footer_padding_length = 0;
-		footer_length = window->width;
-	}
-	cursor_move_horizontal(horizontal_offset);
-	printf("%s", border.corner_bottom_left);
-
-	for (int _ = 0; _ < footer_padding_length; _++) {
-		printf("%s", border.side_bottom);
-	}
-	printf("%.*s", footer_length, window->footer);
-
-	printf("%s\033[0m\n", border.corner_bottom_right);
+	render_window_bottom_border(
+		border, window->footer,
+		window->_internal_rendered_width, horizontal_offset
+	);
 }
 
 void render_frame(const wi_session* session) {
@@ -98,6 +129,8 @@ void render_frame(const wi_session* session) {
 	int max_row_height;
 
 	wi_window* window;
+
+	/*calculate_window_widths(session);*/
 
 	for (int row = 0; row < session->_internal_amount_rows; row++) {
 		accumulated_row_width = 0;
@@ -107,7 +140,7 @@ void render_frame(const wi_session* session) {
 			window = session->windows[row][col];
 
 			if (window->width == -1 && col < session->_internal_amount_cols[row]) {
-				window->width = get_terminal_size().cols - accumulated_row_width - 2;
+				window->_internal_rendered_width = get_terminal_size().cols - accumulated_row_width - 2;
 			}
 
 			render_window(window, accumulated_row_width);
@@ -138,17 +171,20 @@ int main(void)
 	free(window1->title);
 	window1->title = strdup(" This is a nice title ");
 	window1->width = 40;
+	window1->_internal_rendered_width = 40;
 
 	window2->border.focussed_colour = "\033[92m";
 	window2->width = -1;
+	window2->_internal_rendered_width = -1;
 
 	window3->border.focussed_colour = "\033[91m";
 	free(window3->title);
 	window3->title = strdup(" The green window has the standard title ");
 	window3->width = -1;
+	window3->_internal_rendered_width = -1;
 	window3->height = 15;
 
-	render_frame(session);
+	time_it(render_frame(session));
 
 	wi_free_session_completely(session);
 
